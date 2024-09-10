88f9a48cd14fe6dac977d95b7827a4dcd431ade7
BrianSamuel2004 <112035487+BrianSamuel2004@users.noreply.github.com>
2024-09-09 21:31:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Body from "./components/Body"
 import Footer from "./components/Footer";
+import MainSection from "./components/MainSection";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Body />
+      <MainSection/>
       <Footer/>
     </div>
   );
